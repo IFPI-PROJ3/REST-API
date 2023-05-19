499f868234cdd685e148698b6cfebe9bacd0fdd6
Aroldo-Jales <aroldo_jales@hotmail.com>
2023-05-19 12:08:20 -0300

    test: add unit and integrated test project

---
+++ test/Proj3.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Proj3.Domain.Entities.Authentication;
+
+namespace Proj3.IntegrationTests
+{
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            //var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: "WeChangeTestDb" + DateTime.UtcNow).Options;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+
+            //var newUserVolunteer = User.NewUserVolunteer("David Bowie", "davidbowie@email.com");
+            
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ test/Proj3.UnitTests/Application/Utils/Authentication/CryptoTest.cs
@@ -0,0 +1,41 @@
+﻿using Proj3.Application.Utils.Authentication;
+
+namespace Proj3.UnitTests.Application.Utils.Authentication
+{
+    public class CryptoTest
+    {        
+        [Fact(DisplayName = "Get random salt")]
+        public void GetSalt()
+        {
+            // Arrange
+            var salt = Crypto.GetSalt;
+
+            // Act
+            var result = Crypto.GetSalt;
+
+            // Assert
+            Assert.NotEqual(salt, result);
+        }
+
+        [Fact(DisplayName = "Password hashing")]
+        public void PasswordHashing()
+        {
+            // Arrange
+            var password = "P@ssword#69";
+            var salt = Crypto.GetSalt;
+
+            var user = new Domain.Entities.Authentication.User
+            {
+                Email = "user@email.com",
+                Salt = salt
+            };
+
+            // Act            
+            var result = Crypto.ReturnUserHash(user, password);
+            var expected = Crypto.ReturnUserHash(new Domain.Entities.Authentication.User { Email = "user@email.com", Salt = salt}, password);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
